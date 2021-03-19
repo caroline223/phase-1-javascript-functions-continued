2147b4dccd11253057f829ea9dee06596a65f73c
caroline223 <carolineforrester22@gmail.com>
2021-03-18 20:59:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    // Unclear as to why I cannot use console.log() instead of return
+}
+saturdayFun();
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(feeling="*"){
+    return function(emphatic="special"){
+        return `You are ${feeling}${emphatic}${feeling}!`
+    }
+}
